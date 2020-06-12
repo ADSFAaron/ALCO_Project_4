d9114e47d3b5f66103a5a7f9f11565d1057bfb30
ADSF <48749140+ADSFAaron@users.noreply.github.com>
2020-06-12 22:59:00 +0800

    Add files via upload

---
+++ Project 4/Source.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <utility>
+using namespace std;
+
+//目前正在運行的cycle
+static int cycleNo = 0;
+
+/* 每個operator 的cycle數
+*
+*	+ : 2
+*	- : 2
+*	* : 10
+*	/ : 40
+*/
+static int needCycle[4] = { 2,2,8,40 };
+
+struct Opcode
+{
+	string name;
+	string rd;
+	string rs1;
+	string rs2;		// 在I-type當作 immediate
+	char operate;
+};
+
+struct RS
+{
+	bool use;
+	string rs;
+	char operate;
+	int value1;
+	int value2;
+	int cyclenow;
+};
+
+void printCycle(vector<pair<int, string>>& rat, vector<RS>& rsADDSUB, vector<RS>& rsMULDIV, vector<pair<int, int>>& rf)
+{
+	cout << "____RF____" << endl;
+
+	for (int i = 0; i < rf.size(); ++i)
+	{
+		cout << "F" << rf[i].first << " | " << rf[i].second << " |" << endl;
+	}
+
+	cout << "____RAT_____" << endl;
+
+	for (int i = 0; i < rat.size(); ++i)
+	{
+		cout << "F" << rat[i].first << " | " << rat[i].second << " |" << endl;
+	}
+
+	cout << "____RS_____" << endl;
+
+	for (int i = 0; i < rsADDSUB.size(); ++i)
+	{
+		cout << rs[i].rs << " | " << rs[i].operate << " | " << rs[i].value1 << " | " << rs[i].value2 << " | " << endl;
+
+		if (i == 2)
+		{
+			for (int j = 0; j < 30; ++j)
+			{
+				cout << "-";
+			}
+			cout << "Buffered : " << endl;
+
+		}
+		else if (i == 4)
+		{
+			for (int j = 0; j < 30; ++j)
+			{
+				cout << "-";
+			}
+			cout << "Buffered : " << endl;
+		}
+	}
+}
+
+void inputRS(Opcode& opcode, vector<RS>& rs, vector<pair<int, int>>& rf,vector<pair<int, string>>& rat)
+{
+	
+	RS temp;
+	temp.operate = opcode.operate;
+	int rd = opcode.rd[1]-'0';
+	int rs1 = opcode.rs1[1] - '0';
+}
+
+
+
+
+void SplitInstruction(Opcode& opcode, string& input)
+{
+	stringstream ss(input);
+
+	getline(ss, opcode.name, ' ');
+	getline(ss, opcode.rd, ',');
+	getline(ss, opcode.rs1, ',');
+	getline(ss, opcode.rs2);
+}
+
+int main()
+{
+	vector<Opcode> instruction;			//Instruction Queue 用
+	Opcode opInput;
+	string input;						//input handler
+	vector<pair<int, string>> rat;		//Registration Source Table
+	vector<pair<int, int>> rf;			//Register File
+	vector<RS> rsADDSUB, rsMULDIV;		//rs for three add or sub, two for mul and div
+
+	//將對應的RF值存入
+	for (int i = 0; i < 5; ++i)
+	{
+		rf.push_back(make_pair<int, int>(i + 1, 2 * i));
+	}
+
+	//RF
+	for (int i = 0; i < 5; ++i)
+	{
+		rat.push_back(make_pair<int, string>(i + 1, ""));
+	}
+
+	while (true)
+	{
+		getline(cin, input);
+
+		if (input != "exit")
+		{
+			SplitInstruction(opInput, input);
+
+			instruction.push_back(opInput);
+		}
+		else
+		{
+			break;
+		}
+	}
+	int i = 0;
+	do
+	{
+		cycleNo++;
+		if (rsADDSUB.size() < 3) {
+			if (instruction[i].name == "ADD" || instruction[i].name == "ADDI")
+			{
+				instruction[i].operate = '+';
+				inputRS(instruction[i], rsADDSUB, rf, rat);
+			}
+			else if (instruction[i].name == "SUB" || instruction[i].name == "SUBI")
+			{
+				instruction[i].operate = '-';
+				SUB();
+			}
+			i++;
+		}
+		else if (rsMULDIV.size() < 2) {
+			if (instruction[i].name == "MUL" || instruction[i].name == "MULI")
+			{
+				instruction[i].operate = '*';
+				MUL();
+			}
+			else if (instruction[i].name == "DIV" || instruction[i].name == "DIVI")
+			{
+				instruction[i].operate = '/';
+				DIV();
+			}
+			i++;
+		}
+	} while (!rsADDSUB.empty() && !rsMULDIV.empty());
+
+
+
+	system("pause");
+	return 0;
+}
